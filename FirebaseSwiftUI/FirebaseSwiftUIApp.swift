250734957c42959d430bfabc0d684672fdefabcb
Fedotov <sasha4936@yandex.ru>
2023-11-11 17:50:06 +0500

    Initial Commit

---
+++ FirebaseSwiftUI/FirebaseSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseSwiftUIApp.swift
+//  FirebaseSwiftUI
+//
+//  Created by Fedotov Aleksandr on 11.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
